735c315c6d6ebc492e1caa30ce885708a4808570
leonurium <leorangga30@ymail.com>
2026-05-30 17:36:36 +0700

    fix

--- dist/embedding/embedding.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class EmbeddingService {
-    private readonly logger;
-    private readonly baseUrl;
-    private readonly apiKey;
-    constructor();
-    embed(text: string, model?: string): Promise<number[]>;
-}
